3dccf5fc5d12138a7e83ed1e31fcc3b95ce44773
Petros G. Sideris <pgsideris@gmail.com>
2026-04-14 23:57:30 +0100

    Deploy to bsepolia

--- sdk/src/deployments.ts
+++ sdk/src/deployments.ts
@@ -6,5 +6,9 @@ export const DEPLOYMENTS: Record<string, Record<string, HexAddress>> = {
   "31337": {
     "DataRoom": "0x5fc8d32690cc91d4c39d9d3abcbd16989f875707",
     "SecretsVault": "0xa513e6e4b8f2a923d98304ec87f64353c4d5c853"
+  },
+  "84532": {
+    "DataRoom": "0x446a65dd1841d8cedc08aaa61c3163af565270b9",
+    "SecretsVault": "0xbde428ed6be43b03249d217cf0c6f7865396982e"
   }
 } as const;
